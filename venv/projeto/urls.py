9c316d50268103543f68727103986c20e250bfd3
Felipe Travi <fltravi@gmail.com>
2022-05-03 13:49:16 -0300

    adding urls

--- venv/projeto/urls.py
+++ venv/projeto/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from http.client import HTTPResponse
 from django.contrib import admin
 from django.urls import path
 
+def my_view(request):
+    return HTTPResponse('Vai tomar no cu.')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),  
+    path('sobre/', my_view)
 ]
