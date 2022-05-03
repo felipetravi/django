cd08b9b6d2b84cdcaf89c7c5cf52acee922a4022
Felipe Travi <fltravi@gmail.com>
2022-05-03 13:32:27 -0300

    urls

--- venv/projeto/urls.py
+++ venv/projeto/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def my_view():
+    ...
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
